ee55a83b907d6be165dc342110c4f5a78e0e8a44
Thomas Lenormand <lenormand.thomas76110@gmail.com>
2022-04-17 12:40:14 +0000

    remove ensure line

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,12 +1,10 @@
 # change ssh_config file
 file_line { 'ssh_config PasswordAuthentication':
-  ensure => 'present',
   path   => '/etc/ssh/ssh_config',
   line   => '    PasswordAuthentication no',
 }
 
-file_line { 'ssh_config PasswordAuthentication':
-  ensure => 'present',
+file_line { 'ssh_config IdentityFile':
   path   => '/etc/ssh/ssh_config',
   line   => '    IdentityFile ~/.ssh/holberton',
 }
