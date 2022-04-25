2bd859b269f50b3d34d728f715fb7682949c4daa
Thomas Lenormand <lenormand.thomas76110@gmail.com>
2022-04-25 16:26:21 +0000

    change method

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -7,10 +7,9 @@ exec {'apt install -y nginx':
   path => ['/usr/bin'],
 }
 
-file_line { '/etc/nginx/sites-available/default':
-  path => '/etc/nginx/sites-available/default',
-  line => '    add_header X-Served-By $(hostname);',
-}
+nginx::nginx_vhosts:
+  add_header:
+    X-Served-By: $(hostname)
 
 exec {'service nginx restart':
   path => ['/usr/bin'],
