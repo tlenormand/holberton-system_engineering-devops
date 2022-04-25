1444049ebf09b81216c6c2d045d67f94f76f05f7
Thomas Lenormand <lenormand.thomas76110@gmail.com>
2022-04-25 16:29:21 +0000

    add command

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,16 +1,20 @@
 # Add a custom HTTP header with Puppet
 exec {'apt update -y':
-  path => ['/usr/bin'],
+  command => 'apt update -y'
+  path    => ['/usr/bin'],
 }
 
 exec {'apt install -y nginx':
-  path => ['/usr/bin'],
+  command => 'apt install -y nginx'
+  path    => ['/usr/bin'],
 }
 
-nginx::nginx_vhosts:
-  add_header:
-    X-Served-By: $(hostname)
+file_line { '/etc/nginx/sites-available/default':
+  path => '/etc/nginx/sites-available/default',
+  line => '    add_header X-Served-By $(hostname);',
+}
 
 exec {'service nginx restart':
-  path => ['/usr/bin'],
+  command => 'service nginx restart'
+  path    => ['/usr/bin'],
 }
